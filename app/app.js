bbdd6a0a09d3b49f3f1fef3ccd23b079d7d2c832
Raphael Luckom <raphaelluckom@gmail.com>
2016-07-21 02:21:58 +0000

    working pin display

--- app/app.js
+++ app/app.js
@@ -1,8 +1,63 @@
 require('./scss/app.scss');
+var $ = require('jquery');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var Gpios = React.createClass({
+  getInitialState: function() {
+    return [];
+  },
+  handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+  },
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  render: function() {
+    return (
+            <div className="Gpios"></div>
+    );
+  }
+});
+
+var GpioList = React.createClass({
+  loadgpiosFromServer: function() {
+    $.ajax({
+      url: 'http://192.168.1.123/sqlite3/gpio_pins',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: [{pin_number: 'foo'}, {pin_number: 'bar'}]};
+  },
+  componentDidMount: function() {
+    this.loadgpiosFromServer();
+    setInterval(this.loadgpiosFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    var pinNodes = this.state.data.map(function(pin) {
+      return (
+        <div key={pin.pin_number} className="gpioPin">
+          <span className="pinName">{pin.pin_number}</span>
+        </div>
+      );
+    });
+    return (
+      <div className="gpioList">
+        {pinNodes}
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>Hello, cruel world!</h1>,
+  <GpioList pollInterval={1000}></GpioList>,
   document.getElementById('example')
 );
