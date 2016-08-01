e4c4cdbd64098a2f9e2d9d86d7194051594d8ec5
Raphael Luckom <raphaelluckom@gmail.com>
2016-08-01 00:03:04 -0400

    basic controller to keep plants alive

--- gpio/gpio.js
+++ gpio/gpio.js
@@ -1,10 +1,28 @@
 var gpio = require('pigpio').Gpio;
+var moment = require('moment');
+const logger = require('../utils/logger')( '/../logs/gpio.log');
 
-var relay = new gpio(14, {mode: gpio.OUTPUT, pullUpDown:gpio.PUD_DOWN});
+var pump = new gpio(14, {mode: gpio.OUTPUT, pullUpDown:gpio.PUD_DOWN});
+var lights = new gpio(4, {mode: gpio.OUTPUT, pullUpDown:gpio.PUD_DOWN});
 
 var on = 0;
 
 setInterval(function() {
-        on = on === 0 ? 1 : 0;
-        relay.digitalWrite(on);
+  try {
+    on = on === 3 ? 0 : on + 1;
+    logger.log('info', 'pump is ' + (on === 0 ? 1 : 0));
+    pump.digitalWrite(on === 0 ? 1 : 0);
+    var start = moment({hour: 5, minute: 30});
+    var end = moment({hour: 20, minute: 30});
+    if (moment().isBetween(start, end)) {
+      logger.log('info', 'lights on');
+      lights.digitalWrite(1);
+    } else {
+      logger.log('info', 'lights off');
+      lights.digitalWrite(0);
+    }
+  } catch(err) {
+    logger.log('error', err.message);
+    logger.log('error', err.stack);
+  }
 }, 60000); 
