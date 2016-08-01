f94026415dfbe465958a6aeec99f88f7b3bffc14
Raphael Luckom <raphaelluckom@gmail.com>
2016-08-01 00:05:16 -0400

    update schema, generate sqlite schema

--- readme.md
+++ readme.md
@@ -9,3 +9,51 @@ sample monit config line
           start program = "/home/pi/workspace/server/start.sh"
             as uid pi and gid pi
           if does not exist then start
+
+
+System Description
+==================
+
+A web interface and persistence layer for a hydroponics system
+
+The hydroponics system is a 5-gallon bucket with hydroponic nutrient solution filling it partway. There is
+a pump in the bucket that sprays the hydroponic solution at the roots of plants growing in holes cut in the
+lid of the bucket. There is a light over the bucket to help the plants grow. I used the plans from 
+[gardenpool](https://gardenpool.org/online-classes/how-to-make-a-simple-5-gallon-bucket-aeroponics-system).
+
+The control system is a raspberry pi. It is responsible for turning the pump on and off (currently one minute on,
+two minutes off) and turning the light on at 5:30AM and off at 8:30PM. 
+
+
+Requirements
+============
+
+I would like to add features to the control system. Specifically:
+
+  1. Remote configuration of lights and pump on / off supportng both cycle-based control (1 on, 2 off) and time-based
+     control (5:30AM on, 8:30PM off)
+  2. Easy addition of features such as camera monitoring, etc.
+  3. Separation of the gpio control process (which pretty much has to run as root) and the interface process
+     (which should not run as root, being available at least over an intranet, possibly over the internet).
+  4. Easy, configuration-based extension of the server and client code.
+
+
+Application Data
+================
+
+There will be at least 3 execution environments for the code in this interface (browser, gpio control process,
+server process) but all of them need to share access to the application's data. This data will include identifying
+gpio pins, specifying their duty cycles, and identifying the type of duty cycle. 
+
+Instead of rewriting the data access code for each program that needs it, I'm writing some code to parse a data
+structure representing the layout of the database and generate a database schema, a server-side API, and an API
+client to be used as the model layer in the browser. This code will enable data access from any enviroment, as well
+as data validation on saves and easy extensibility.
+
+
+Technologies
+============
+
+I have chosen sqlite3 because it's lightweight, well-supported, and I'm familiar with it.
+
+I've chosen JavaScript because I like it, and also because it's native to the browser.
--- utils/schema.js
+++ utils/schema.js
@@ -1,56 +1,222 @@
 'use strict';
+var _ = require('lodash');
+
+/* sequence name is unique
+ * sequence uid is unique
+ * sequence type exists in sequenceTypes
+ */
+function validateSequence(sequence, sequencesByName, sequencesByUid, sequenceTypes) {
+  return (
+    // sequencesByName is falsy or has length 0
+    (!sequencesByName || (sequencesByName.length === 0))
+    // sequencesByUid is falsy or has length 0
+    && (!sequencesByUid || (sequencesByUid.length === 0))
+  // name and uid are defined
+    && sequence.name && sequence.uid
+    && _.find(sequenceTypes, 'sequenceId', sequence.sequenceType)
+  );
+}
+validateSequence.restParams = ['sequencesByName', 'sequencesByUid'];
+
+/* gpioPins can't be created but they can be modified.
+ * Only the sequenceUid can be changed.
+ */
+function validateGpioPins(gpioPin, gpioPinByUid) {
+  return (
+    _.values(gpioPin).length === _.values(gpioPinByUid).length
+    && _.every(_.values(this.columns), function(n) {
+      return n === 'sequenceUid' ? true : gpioPin[n] === gpioPinByUid[n];
+    })
+  );
+}
+
+/* uid is unique
+ * if sequenceType is duration, durationSeconds is defined and startTime and endTime are not.
+ * if sequenceType is time, durationSeconds is not defined and startTime and endTime are.
+ * if sequenceType is duration, there are no sequenceItems with the same ordinal and sequenceUid
+ * if sequenceType is time, there are no sequenceItems in the same sequence that the startTime or endTime falls in.
+ * state is defined
+ */
+function sequenceItems(sequenceItem, sequenceByUid, sequenceTypes, sequenceItemsBySequenceUid) {
+  const sequenceTypeName = _.find(sequenceTypes, 'sequenceId', sequenceByUid.sequenceType).sequenceTypeName;
+  const durationOk = (
+    sequenceTypeName === 'TIME'
+    || (
+      !_.any(sequenceItemsBySequenceUid, 'ordinal', sequenceItem.ordinal)
+      && (_.isNumber(sequenceItem.durationSeconds) 
+      && !sequenceItem.startTime
+      && !sequenceItem.endTime
+      )
+    ));
+  const timeOk = (
+    sequenceTypeName === 'DURATION'
+  )
+}
 
 function schemaFactory(noOpValidate) {
   return  {
     'sequences': {
       id: 'uid',
-      columnNames: ['uid', 'dateCreated', 'defaultState', 'name'],
-      validateAndSave: noOpValidate, 
-      GET: true,
-      POST: true,
-      PUT: true,
-      DELETE: true,
+      columns: {
+        'uid': 'TEXT',
+        'dateCreated': 'TEXT',
+        'sequenceType': 'NUMBER',
+        'defaultState': 'TEXT'
+      },
+      apiMethods: {
+        GET: true,
+        POST: true,
+        PUT: true,
+        DELETE: true
+      }
     },
     'gpioPins': {
       id: 'pinNumber',
-      columnNames: ['pinNumber', 'sequenceUid'],
-      validateAndSave: noOpValidate, 
-      GET: true,
-      POST: false,
-      PUT: true,
-      DELETE: false
+      columns: {
+        'pinNumber': 'NUMBER', 
+        'sequenceUid': 'TEXT',
+      },
+      apiMethods: {
+        GET: true,
+        POST: false,
+        PUT: true,
+        DELETE: false
+      },
+      constraints: {
+        FOREIGN_KEYS: {
+          sequenceUid : 'sequences.uid'
+        }
+      },
+      initialValues: [
+        {pinNumber: 14, sequenceUid: null},
+        {pinNumber: 15, sequenceUid: null},
+        {pinNumber: 18, sequenceUid: null},
+        {pinNumber: 23, sequenceUid: null},
+        {pinNumber: 24, sequenceUid: null},
+        {pinNumber: 25, sequenceUid: null},
+        {pinNumber: 8, sequenceUid: null},
+        {pinNumber: 7, sequenceUid: null},
+        {pinNumber: 2, sequenceUid: null},
+        {pinNumber: 3, sequenceUid: null},
+        {pinNumber: 4, sequenceUid: null},
+        {pinNumber: 17, sequenceUid: null},
+        {pinNumber: 27, sequenceUid: null},
+        {pinNumber: 22, sequenceUid: null},
+        {pinNumber: 10, sequenceUid: null},
+        {pinNumber: 9, sequenceUid: null},
+        {pinNumber: 11, sequenceUid: null}
+      ]
     },
     'sequenceItems': {
       id: 'uid',
-      columnNames: [
-        'uid',
-        'dateCreated',
-        'sequenceUid',
-        'sequenceType',
-        'durationSeconds',
-        'ordinal',
-        'startTime',
-        'endTime',
-        'state'
-      ],
-      validateAndSave: noOpValidate, 
-      GET: true,
-      POST: true,
-      PUT: true,
-      DELETE: true
+      columns: {
+        'uid': 'TEXT',
+        'dateCreated': 'TEXT',
+        'sequenceUid': 'TEXT',
+        'durationSeconds': 'NUMBER',
+        'ordinal': 'NUMBER',
+        'startTime': 'TEXT',
+        'endTime': 'TEXT',
+        'state': 'TEXT'
+      },
+      apiMethods: {
+        GET: true,
+        POST: true,
+        PUT: true,
+        DELETE: true
+      },
+      constraints: {
+        FOREIGN_KEYS: {
+          sequenceUid: 'sequences.uid',
+          sequenceType: 'sequenceTypes.sequenceId'
+        },
+        UNIQUE: [['sequenceUid', 'ordinal']],
+      }
     },
     'sequenceTypes': {
       id: 'sequenceId',
-      columnNames: ['sequenceId', 'sequenceTypeName'],
-      validateAndSave: noOpValidate, 
-      GET: true,
-      POST: false,
-      PUT: false,
-      DELETE: false
+      columns: {
+        'sequenceId': 'NUMBER',
+        'sequenceTypeName': 'TEXT'
+      },
+      apiMethods: {
+        GET: true,
+        POST: false,
+        PUT: false,
+        DELETE: false
+      },
+      constraints: {
+        UNIQUE: [['sequenceTypeName']],
+      },
+      initialValues: [
+        {sequenceId: 1, sequenceTypeName: 'DURATION'},
+        {sequenceId: 2, sequenceTypeName: 'TIME'}
+      ]
     }
   };
 }
 
+function buildSqliteSchema(schema) {
+  var finishedTables = [];
+  var tableDependencies = _.reduce(schema, function(acc, val, key) {
+    var dependencies = [];
+    var foreignKeyConstraints = _.get(val, 'constraints.FOREIGN_KEYS');
+    if (foreignKeyConstraints) {
+      _.each(foreignKeyConstraints, function(v, k) {
+        // v  e.g.=  'sequences.uid'
+        dependencies.push(v.split('.')[0]);
+      });
+      acc[key] = dependencies;
+    }
+    return acc;
+  }, {});
+  var statements = '';
+  while (finishedTables.length !== _.keys(schema).length) {
+    _.each(schema, function(tableDescription, tableName) {
+      var unmetDependencies = _.some(
+        tableDependencies[tableName], function(dependencyName) {
+          return finishedTables.indexOf(dependencyName) === -1;
+        }
+      );
+      if (finishedTables.indexOf(tableName) !== -1 || unmetDependencies) {
+        return;
+      }
+      var createStatement = `CREATE TABLE IF NOT EXISTS ${tableName} (\n`;
+      var statementBodyList = [];
+      _.each(tableDescription.columns, function(type, name) {
+        statementBodyList.push(`  ${name} ${type}${tableDescription.id === name ? ' PRIMARY KEY' : ''}`);
+      });
+
+      _.each(_.get(tableDescription, 'constraints.UNIQUE'), function(uniqueList) {
+        statementBodyList.push(`  UNIQUE (${uniqueList.join(', ')})`);
+      });
+
+      _.each(_.get(tableDescription, 'constraints.FOREIGN_KEY'), function(selfColumn, otherColumn) {
+        statementBodyList.push(`  FOREIGN KEY(${selfColumn}) REFERENCES ${otherColumn.split('.')[0]}(${otherColumn.split('.')[1]})`);
+      });
+      createStatement += `${statementBodyList.join(',\n')}\n);\n\n`;
+      statements += createStatement;
+      _.each(tableDescription.initialValues, function(val, indx) {
+        var values = _.map(_.values(val), (v) => {
+          if (_.isUndefined(v) || _.isNull(v)) {
+            return 'null';
+          } else if (_.isString(v)) {
+            return '"' + v + '"';
+          } else {
+            return v;
+          }
+        });
+        statements += `INSERT OR ABORT INTO ${tableName} (${_.keys(val).join(', ')}) VALUES (${values.join(', ')});\n${indx === tableDescription.initialValues.length - 1 ? '\n' : ''}`;
+      });
+      finishedTables.push(tableName);
+    });
+  }
+  return statements;
+}
+
+console.log(buildSqliteSchema(schemaFactory()));
+
 module.exports = {
-  schemaFactory: schemaFactory
+  schemaFactory: schemaFactory,
+  buildSqliteSchema: buildSqliteSchema
 };
