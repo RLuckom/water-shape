f7c907d7407aeb9dad3ca51c8c4db3cb6910e19e
Raphael Luckom <RaphaelLuckom@gmail.com>
2016-08-10 23:20:52 -0400

    fix schema fixture

--- spec/fixtures/schema.sql
+++ spec/fixtures/schema.sql
@@ -4,8 +4,8 @@ CREATE TABLE IF NOT EXISTS sequenceTypes (
   UNIQUE (sequenceTypeName)
 );
 
-INSERT OR ABORT INTO sequenceTypes (sequenceId, sequenceTypeName) VALUES (1, "DURATION");
-INSERT OR ABORT INTO sequenceTypes (sequenceId, sequenceTypeName) VALUES (2, "TIME");
+INSERT OR IGNORE INTO sequenceTypes (sequenceId, sequenceTypeName) VALUES (1, "DURATION");
+INSERT OR IGNORE INTO sequenceTypes (sequenceId, sequenceTypeName) VALUES (2, "TIME");
 
 CREATE TABLE IF NOT EXISTS sequences (
   uid TEXT PRIMARY KEY,
@@ -21,23 +21,23 @@ CREATE TABLE IF NOT EXISTS gpioPins (
   FOREIGN KEY(sequenceUid) REFERENCES sequences(uid)
 );
 
-INSERT OR ABORT INTO gpioPins (pinNumber, sequenceUid) VALUES (14, null);
-INSERT OR ABORT INTO gpioPins (pinNumber, sequenceUid) VALUES (15, null);
-INSERT OR ABORT INTO gpioPins (pinNumber, sequenceUid) VALUES (18, null);
-INSERT OR ABORT INTO gpioPins (pinNumber, sequenceUid) VALUES (23, null);
-INSERT OR ABORT INTO gpioPins (pinNumber, sequenceUid) VALUES (24, null);
-INSERT OR ABORT INTO gpioPins (pinNumber, sequenceUid) VALUES (25, null);
-INSERT OR ABORT INTO gpioPins (pinNumber, sequenceUid) VALUES (8, null);
-INSERT OR ABORT INTO gpioPins (pinNumber, sequenceUid) VALUES (7, null);
-INSERT OR ABORT INTO gpioPins (pinNumber, sequenceUid) VALUES (2, null);
-INSERT OR ABORT INTO gpioPins (pinNumber, sequenceUid) VALUES (3, null);
-INSERT OR ABORT INTO gpioPins (pinNumber, sequenceUid) VALUES (4, null);
-INSERT OR ABORT INTO gpioPins (pinNumber, sequenceUid) VALUES (17, null);
-INSERT OR ABORT INTO gpioPins (pinNumber, sequenceUid) VALUES (27, null);
-INSERT OR ABORT INTO gpioPins (pinNumber, sequenceUid) VALUES (22, null);
-INSERT OR ABORT INTO gpioPins (pinNumber, sequenceUid) VALUES (10, null);
-INSERT OR ABORT INTO gpioPins (pinNumber, sequenceUid) VALUES (9, null);
-INSERT OR ABORT INTO gpioPins (pinNumber, sequenceUid) VALUES (11, null);
+INSERT OR IGNORE INTO gpioPins (pinNumber, sequenceUid) VALUES (14, null);
+INSERT OR IGNORE INTO gpioPins (pinNumber, sequenceUid) VALUES (15, null);
+INSERT OR IGNORE INTO gpioPins (pinNumber, sequenceUid) VALUES (18, null);
+INSERT OR IGNORE INTO gpioPins (pinNumber, sequenceUid) VALUES (23, null);
+INSERT OR IGNORE INTO gpioPins (pinNumber, sequenceUid) VALUES (24, null);
+INSERT OR IGNORE INTO gpioPins (pinNumber, sequenceUid) VALUES (25, null);
+INSERT OR IGNORE INTO gpioPins (pinNumber, sequenceUid) VALUES (8, null);
+INSERT OR IGNORE INTO gpioPins (pinNumber, sequenceUid) VALUES (7, null);
+INSERT OR IGNORE INTO gpioPins (pinNumber, sequenceUid) VALUES (2, null);
+INSERT OR IGNORE INTO gpioPins (pinNumber, sequenceUid) VALUES (3, null);
+INSERT OR IGNORE INTO gpioPins (pinNumber, sequenceUid) VALUES (4, null);
+INSERT OR IGNORE INTO gpioPins (pinNumber, sequenceUid) VALUES (17, null);
+INSERT OR IGNORE INTO gpioPins (pinNumber, sequenceUid) VALUES (27, null);
+INSERT OR IGNORE INTO gpioPins (pinNumber, sequenceUid) VALUES (22, null);
+INSERT OR IGNORE INTO gpioPins (pinNumber, sequenceUid) VALUES (10, null);
+INSERT OR IGNORE INTO gpioPins (pinNumber, sequenceUid) VALUES (9, null);
+INSERT OR IGNORE INTO gpioPins (pinNumber, sequenceUid) VALUES (11, null);
 
 CREATE TABLE IF NOT EXISTS sequenceItems (
   uid TEXT PRIMARY KEY,
