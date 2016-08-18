3b8b1ee33f62694974c5477923f928f3a9d8a1e8
Raphael Luckom <RaphaelLuckom@gmail.com>
2016-08-18 01:10:01 -0400

    update gpio

--- gpio/gpio.js
+++ gpio/gpio.js
@@ -1,6 +1,22 @@
 'use strict';
 var gpio = require('pigpio').Gpio;
 const logger = require('../utils/logger')( '/../logs/gpio.log');
+var sequenceUtilsFactory = require('../utils/sequenceManipulation.js');
 var gpioLibFactory = require('./gpioLib.js');
+const dbUtils = require('./../water-shape/persistence/sqlite3-adapter.js');
+const FILENAME = 'hydro.db';
+const schema = require('../schema/schema.js').schemaFactory();
 
-gpioLibFactory(logger, gpio);
+dbUtils(FILENAME, schema, logger, function(db) {
+  var sequenceUtils = sequenceUtilsFactory(db);
+  function deepSequences(callback) {
+    return sequenceUtils.getSequencesWithItemsAndPins(function(err, res) {
+      if (err) {
+        throw err;
+      } else {
+        return callback(res);
+      }
+    });
+  }
+  gpioLibFactory(logger, gpio, deepSequences).start();
+}
