6699c7a41fa89aa7c9dca63fc4378be6ff8d2ed2
Raphael Luckom <RaphaelLuckom@gmail.com>
2016-10-03 04:17:45 -0400

    give phantom test more time

--- spec/phantom/home/ui.spec.js
+++ spec/phantom/home/ui.spec.js
@@ -6,14 +6,16 @@ describe('ui tests', function() {
 
 
   it('serves the page', function(done) {
-    driver.sleep(9000).then(function() {
-      driver.findElement(by.id('example')).then(function(e) {
-        return e.getText().then(function(t) {
-          console.log('t', t);
+    driver.findElement(by.id('example')).then(function(e) {
+      return e.getText().then(function(t) {
+        console.log('t', t);
+        if (!t) {
+          fail('must see text on page');
+        } else {
           expect(t.indexOf('Raspberry Pi GPIO Scheduling Controller') !== -1).toBe(true);
-          return done();
-        });
+        }
+        return done();
       });
     });
-  });
+  }, 30000);
 });
