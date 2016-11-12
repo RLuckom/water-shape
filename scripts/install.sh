75b8c0ccefcc7ebb43dab6f3cb33d38a7026cc99
Raphael Luckom <RaphaelLuckom@gmail.com>
2016-11-12 14:29:39 -0500

    Created bin directory for entry points
    moved server start script into bin directory
    created scheduler start script
    
    There's still work to be done getting the monit scripts, etc updated.

---
+++ scripts/install.sh
@@ -0,0 +1,5 @@
+sudo apt-get install build-essential libssl-dev
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+source ~/.bashrc
+nvm install v6
+sudo apt-get install monit
